f95ed16fa0daa8e0888ace1fb1b7881b23939b22
Manoj Kumar <manoj.kb2011@gmail.com>
2018-07-20 22:53:59 +0530

    few addon in doubly linkedlist

--- src/custom/CustomDoublyLinkedList.java
+++ src/custom/CustomDoublyLinkedList.java
@@ -9,14 +9,82 @@ public class CustomDoublyLinkedList<E> {
 		customDoublyLinkedList.addFirst(10);
 		customDoublyLinkedList.addFirst(34);
 		customDoublyLinkedList.addLast(56);
-		customDoublyLinkedList.addLast(364);
+
+		customDoublyLinkedList.addLast(57);
+
+		customDoublyLinkedList.addLast(58);
+		customDoublyLinkedList.addLast(94);
 		
 		customDoublyLinkedList.iterateForward();
+	
+		//customDoublyLinkedList.delete(58);
 		
+		System.out.println("==");
+		customDoublyLinkedList.iterateForward();
 		
+		System.out.println("reverse double linkedlist====");
+		customDoublyLinkedList.reverse();
+		customDoublyLinkedList.iterateForward();
 	}
 
 	
+	private void reverse() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is reversing process");
+		Node current = head;
+		Node temp = null;
+		
+		while (current!=null) {
+		temp = current.prev;
+		current.prev =  current.next;
+		current.next = temp;
+		current = current.prev;
+		
+		}
+		
+		if (temp!=null) {
+			head = temp.prev;
+		}
+		
+		System.out.println("completed reverse process");
+	}
+
+
+	private void delete(E i) {
+		// TODO Auto-generated method stub
+		Node tmp = head;
+		while (tmp!=null) {
+			if (tmp.element==i) {
+				E el = (E) tmp.element;
+				
+				if (tmp.prev==null) {
+					head = tmp.next;
+					head.prev = null;
+					
+					break;
+				}else if(tmp.next==null){
+					Node lNode = tail;
+					tail = tail.prev;
+					tail.next = null;
+					break;
+				}else{
+					Node nextN = tmp.next;	
+					Node preN = tmp.prev;	
+					preN.next = nextN;	
+					break;	
+				}
+				
+				
+		
+			}
+			
+			tmp = tmp.next;
+			
+		}
+	}
+
+
 	private void iterateForward() {
 		System.out.println("Iterate forward");
 		Node tmp = head;
