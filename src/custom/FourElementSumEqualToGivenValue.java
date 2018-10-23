38d32f31d4119e8c726d3d443818a5f5fd3aa460
Manoj <mbaghela1@xavient.com>
2018-10-23 17:33:05 +0530

    few changes

--- src/custom/FourElementSumEqualToGivenValue.java
+++ src/custom/FourElementSumEqualToGivenValue.java
@@ -2,31 +2,44 @@ package custom;
 
 import java.lang.reflect.Array;
 import java.util.HashMap;
+import java.util.Scanner;
 import java.util.Vector;
 
 public class FourElementSumEqualToGivenValue {
 
 	public static void main(String[] args) {
-		FourElementSumEqualToGivenValue obj = new FourElementSumEqualToGivenValue();
-		int[] arr = {1, 5, 1, 0, 6, 0};
-		int sum = 7;
+		//FourElementSumEqualToGivenValue obj = new FourElementSumEqualToGivenValue();
+		//int[] arr = {1, 5, 1, 0, 6, 0};
+		//int sum = 7;
 		
 		
 		//int[] arr = {38, 7, 44 ,42, 28, 16, 10, 37, 33, 2, 38, 29,26, 8, 25};
 		//int sum = 22;
 		
 		
-		obj.isSumofFourElemenr(arr, sum);
+		//obj.isSumofFourElemenr(arr, sum);
+		
+		Scanner kb=new Scanner(System.in);
+		int t=kb.nextInt();
+		for (int i = 0; i < t; i++) {
+			int arraysize = kb.nextInt();
+			int[] arr = new int[arraysize];
+			for (int j = 0; j < arraysize; j++) {
+				arr[j] = kb.nextInt();
+			}
+			
+			int sum = kb.nextInt();
+			isSumofFourElemenr(arr, sum);
+		}
 	}
 
-	private void isSumofFourElemenr(int[] arr, int sum) {
+	private static void isSumofFourElemenr(int[] arr, int sum) {
 
 		HashMap<Integer, int[]> storeHash = new HashMap<Integer, int[]>();
 				
 				for (int i = 0; i < arr.length; i++) {
 					for (int j = i+1; j < arr.length; j++) {
-						
-						
+					
 						int tempSum = arr[i]+arr[j];
 						
 						if (storeHash.containsKey(sum-tempSum)) {
